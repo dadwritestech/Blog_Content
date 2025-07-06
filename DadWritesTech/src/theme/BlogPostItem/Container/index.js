e0664b0f6520315f6e6ea5cac41236596be229b2
dadwritestech <157202737+dadwritestech@users.noreply.github.com>
2025-07-06 18:03:23 +0200

    blog_fix

---
+++ DadWritesTech/src/theme/BlogPostItem/Container/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function BlogPostItemContainer({children, className}) {
+  return <article className={`${className} chaos-card`}>{children}</article>;
+}
